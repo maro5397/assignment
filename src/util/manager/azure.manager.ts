0f81977e068e6e458387bae3e1a78211cfb6e585
reindeer002 <lsj000424@gmail.com>
2025-03-28 06:36:37 +0000

    refactor: put get function for start server

--- src/util/manager/azure.manager.ts
+++ src/util/manager/azure.manager.ts
@@ -1,6 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { ResourceType } from '../../common/type';
 
 @Injectable()
 export class AzureManager {
   private readonly logger = new Logger(AzureManager.name);
+
+  async getRelationships(resource: ResourceType, name: string) {
+    return resource + name;
+  }
+
+  async getPermission(user: string) {
+    return user;
+  }
 }
--- src/util/manager/gcp.manager.ts
+++ src/util/manager/gcp.manager.ts
@@ -1,6 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { ResourceType } from "../../common/type";
 
 @Injectable()
 export class GCPManager {
   private readonly logger = new Logger(GCPManager.name);
+
+  async getRelationships(resource: ResourceType, name: string) {
+    return resource + name;
+  }
+
+  async getPermission(user: string) {
+    return user;
+  }
 }
