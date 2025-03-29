e8f13664dd5c5c653f526d957bc7015ffaf945b0
reindeer002 <lsj000424@gmail.com>
2025-03-29 07:39:35 +0000

    feat: enroll fetcher for azure

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,21 @@ import { AzureManager } from './util/manager/azure.manager';
 import { ConfigModule } from '@nestjs/config';
 import { AwsIamFetcher } from './util/fetcher/aws/aws-iam.fetcher';
 import { AwsLoadBalancingFetcher } from './util/fetcher/aws/aws-load-balancing.fetcher';
+import { AzureNetworkManagementFetcher } from './util/fetcher/azure/azure-network-management.fetcher';
+import { AzureAuthorizationManagementFetcher } from './util/fetcher/azure/azure-authorization-management.fetcher';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [CloudController],
-  providers: [CloudServiceImpl, AWSManager, GCPManager, AzureManager, AwsIamFetcher, AwsLoadBalancingFetcher],
+  providers: [
+    CloudServiceImpl,
+    AWSManager,
+    GCPManager,
+    AzureManager,
+    AwsIamFetcher,
+    AwsLoadBalancingFetcher,
+    AzureNetworkManagementFetcher,
+    AzureAuthorizationManagementFetcher,
+  ],
 })
 export class AppModule {}
