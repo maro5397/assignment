7455612374ee3d499d2d3a50d8f79b7483162ace
reindeer002 <lsj000424@gmail.com>
2025-03-29 07:32:02 +0000

    chore: erase gcp code and add not implement exception

--- src/util/manager/gcp.manager.ts
+++ src/util/manager/gcp.manager.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotImplementedException } from '@nestjs/common';
 import { ResourceType } from '../../common/type';
 
 @Injectable()
@@ -6,10 +6,10 @@ export class GCPManager {
   private readonly logger = new Logger(GCPManager.name);
 
   async getRelationships(resource: ResourceType, name: string) {
-    return resource + name;
+    throw new NotImplementedException(`GCP cloud service is not supported (${resource}, ${name})`);
   }
 
   async getPermission(user: string) {
-    return user;
+    throw new NotImplementedException(`GCP cloud service is not supported (${user})`);
   }
 }
